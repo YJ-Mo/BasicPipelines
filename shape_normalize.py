5bc9c1c399b06ab23fddd2a192c9d92c420c2da7
Y.Mo <46513398+YJ-Mo@users.noreply.github.com>
2021-06-24 16:39:44 +0800

    Update shape_normalize.py

--- shape_normalize.py
+++ shape_normalize.py
@@ -8,13 +8,12 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-##转录本注释和数据读入
-#### path #####
-exon_gtf_path='/data/TA_QUIZ_RNA_regulation/data/ATH/GTF/shape_map/ath_exons.gtf'
-col_uv_f_path='/data/TA_QUIZ_RNA_regulation/data/riboshape_liulab_batch4/final.modified_umodified/col_nouv/'
-col_uv_z_path='/data/TA_QUIZ_RNA_regulation/data/riboshape_liulab_batch4/final.modified_umodified/col_uv/'
+## input ref exons/transcripts and data folder
+exon_gtf_path='your_dir/ath_exons.gtf'
+col_uv_f_path='your_dir/final.modified_umodified/col_nouv/'
+col_uv_z_path='your_dir/final.modified_umodified/col_uv/'
 
-### 读取注释文件，在exon_gtf文件的第8列中提取'transcript_id','gene_type','gene_name','chr'等信息
+## extract ref info from ref
 gtf_data=pd.read_csv(exon_gtf_path,sep='\t',header=None)
 gtf_data_new=pd.DataFrame(columns={'transcript_id','gene_type','gene_name','chr'})
 gtf_data_new['transcript_id']=gtf_data.iloc[:,8].apply(lambda x:x.split(';')[1].split('"')[1])
@@ -24,7 +23,7 @@ gtf_data_new['chr'] = gtf_data.iloc[:,0].apply(lambda x: 6 if x=='Mt' else 7 if 
 gtf_data_new = gtf_data_new.drop_duplicates()
 gtf_data_new.index = range(len(gtf_data_new))
 
-#  提取col_nouv、col_uv中'cutoff.hit.group'的'group','transcript_id','modified.median','unmodified.median','modified.sum','unmodified.sum','hit'列，并将ath_exons参考基因组中的'gene_type'，'gene_name','chr'等信息合并到原数据中
+##  extract data info from data
 
 hit_level_col_uv_f = pd.read_csv(col_uv_f_path+'/cutoff.hit.group',sep='\t')
 hit_level_col_uv_f.columns =['group','transcript_id','modified.median','unmodified.median','modified.sum','unmodified.sum','hit']
@@ -36,8 +35,7 @@ hit_level_col_uv_z.columns =['group','transcript_id','modified.median','unmodifi
 hit_level_col_uv_z = pd.merge(hit_level_col_uv_z,gtf_data_new,on='transcript_id',how='left')
 hit_level_col_uv_z['spe'] = 'WT_UV+'
 
-
-###从col_uv_f、col_uv_z中筛选出gene_type属于['lncRNA','rRNA','tRNA']，hit_level的值>10 && modified.median>5000的转录本，并输出它的转录本ID###
+## select gene type, hit level and modified.median
 col_uv_f_id = hit_level_col_uv_f.loc[(hit_level_col_uv_f.gene_type.isin(['lncRNA','rRNA','tRNA']))&(hit_level_col_uv_f['modified.median']>5000)&(hit_level_col_uv_f['hit']>10),'transcript_id']
 col_uv_z_id = hit_level_col_uv_z.loc[(hit_level_col_uv_z.gene_type.isin(['lncRNA','rRNA','tRNA']))&(hit_level_col_uv_z['modified.median']>5000)&(hit_level_col_uv_z['hit']>10),'transcript_id']
 
