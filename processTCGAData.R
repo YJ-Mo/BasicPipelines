9107450765225e21ccc9214846f82894ec334bf0
Y.Mo <46513398+YJ-Mo@users.noreply.github.com>
2021-06-30 15:31:39 +0800

    Update processTCGAData.R

--- processTCGAData.R
+++ processTCGAData.R
@@ -13,3 +13,17 @@ for (i in files[2:length(files)]){
 }
 write.table(count,paste("/home/user_25/cancer/rawData/TCGA/",seq,"/counts.txt",sep=""),sep='\t',quote=F,row.names=F)
 }
+
+counts=read.table("/home/user_25/cancer/rawData/TCGA/ESCA/counts.txt",header = T,row.names = 1)
+labels=read.table("/home/user_25/cancer/rawData/TCGA/ESCA/ESCA_sample_sheet.2019-06-09.tsv",header = T,sep="\t")
+counts=counts[grep("ENSG",rownames(counts)),]
+name=colnames(counts)
+labels$File.Name=gsub("-",".",labels$File.Name)
+name=gsub("X","",name,ignore.case = FALSE)
+labels=labels[labels$File.Name %in% name,]
+labels$Sample.Type=gsub("Solid Tissue Normal","NC",labels$Sample.Type)
+labels$Sample.Type=gsub("Primary Tumor","ESCA",labels$Sample.Type)
+labels=cbind(labels$File.Name,labels$Sample.Type)
+colnames(labels)=c("sample","label")
+write.table(counts,"/home/user_25/cancer/rawData/TCGA/ESCA/count.txt",quote=F)
+write.table(labels,"/home/user_25/cancer/rawData/TCGA/ESCA/label.txt",quote=F,row.names=F)
