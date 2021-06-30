63a7cbff5917f912baa5c19f2a0f27f13f4b7da9
Y.Mo <46513398+YJ-Mo@users.noreply.github.com>
2021-06-30 19:47:54 +0800

    Update processTCGAData.R

--- processTCGAData.R
+++ processTCGAData.R
@@ -29,13 +29,13 @@ write.table(counts,"/home/user_25/cancer/rawData/TCGA/ESCA/count.txt",quote=F)
 write.table(labels,"/home/user_25/cancer/rawData/TCGA/ESCA/label.txt",quote=F,row.names=F)
 
 seq=c("ESCA","STAD","LUAD","COAD","READ","LIHC")
-all_counts=read.table("/home/user_25/cancer/rawData/TCGA/ESCA/counts.txt",header = T,row.names = 1)
+all_count=read.table("/home/user_25/cancer/rawData/TCGA/ESCA/counts.txt",header = T,row.names = 1)
 all_label=read.table("/home/user_25/cancer/rawData/TCGA/ESCA/label.txt",header = T,sep="\t")
 for (i in 2:length(seq)){
     c_path=paste("/home/user_25/cancer/rawData/TCGA/",seq[i],"/count.txt",sep="")
     l_path=paste("/home/user_25/cancer/rawData/TCGA/",seq[i],"/label.txt",sep="")
     c=read.table(c_path,header = T,row.names = 1)
     l=read.table(l_path,header = T,sep="\t")
-    all_counts=cbind(all_counts,c)
-    
+    all_count=cbind(all_count,c)
+    all_label=rbind(all_label,l)
 }
